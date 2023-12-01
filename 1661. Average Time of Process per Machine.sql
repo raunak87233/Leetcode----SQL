3805bb634a7205d7ce2a172900c2692c0e6df633
Raunak Gupta <106958764+raunak87233@users.noreply.github.com>
2023-12-01 17:46:04 +0530

    Create 1661. Average Time of Process per Machine.sql

---
+++ 1661. Average Time of Process per Machine.sql
@@ -0,0 +1,6 @@
+# Write your MySQL query statement below
+SELECT machine_id,
+ ROUND(SUM(IF(activity_type='start', -timestamp, timestamp)) / COUNT(DISTINCT process_id), 3) AS processing_time
+FROM Activity 
+GROUP BY machine_id
+ORDER BY machine_id
